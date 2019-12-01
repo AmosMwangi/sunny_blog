bfd8805ac0aeb8159b0e4379da9bf297212d934e
AmosMwangi <amosmwangi101@gmail.com>
2019-12-02 02:41:18 +0300

    made some exports like secret_keys, data url

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='amos'
+export APP_SETTINGS="config.DevelopmentConfig"
+export DATABASE_URL='postgresql+psycopg2://ermoh:12345@localhost/blogg'
+export MAIL_USERNAME='browndwane22@gmail.com'
+export MAIL_PASSWORD='dope fam'
+
+python3.6 manage.py server
